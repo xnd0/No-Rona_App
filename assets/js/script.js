eb9a03365c9f1107187f24dd7a34dae3df9f06b2
xndcmd <xndcmd@gmail.com>
2022-06-09 11:57:50 -0700

    successfully got API data to print to console (Chuck Norris API)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,39 @@
 // ---------------------- //
 
 
+
+
+
 // Chuck Norris API URL
-var requestUrl = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random'
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		'X-RapidAPI-Key': '435d50c541mshfc9db86f68d6624p177e00jsn2e5482e44a2b',
+		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+	}
+};
+
+fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+
+// var requestUrl = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random'
 
 
 
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-  });
+// fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log('Fetch Response \n-------------');
+//     console.log(data);
+//   });
+
+
+  
