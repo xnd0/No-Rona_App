f647b35271c0a67ddc83bb63b9875f5a1c2d2183
xndcmd <xndcmd@gmail.com>
2022-06-09 11:49:56 -0700

    added javascript function to fetch chuck norris API data- in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,17 @@
 // ---------------------- //
 
 
+// Chuck Norris API URL
+var requestUrl = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random'
 
+
+
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Fetch Response \n-------------');
+    console.log(data);
+  });
