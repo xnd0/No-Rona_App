57ca8c285cd05a878d214c6323daa749ce0fce2f
xndcmd <xndcmd@gmail.com>
2022-06-13 17:01:48 -0700

    additional streamlining of app functions. County search triggers link display for norris.html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,33 +59,6 @@ function saveBadJoke() {
 
 
 
-// function displaySaved() {
-// 	let adviceList = JSON.parse(localStorage.getItem("chuckAdvice"))
-// 	for (let i = 0; i < adviceList.length; i++) {
-//         const adviceEl = document.createElement("li");
-//         adviceEl.textContent = adviceList[i];
-//         savedQuotes.appendChild(adviceEl); 
-//     }
-
-// 	let dadJokeList = JSON.parse(localStorage.getItem("dadJokeSave"))
-// 	for (let i = 0; i < dadJokeList.length; i++) {
-//         const dadEl = document.createElement("li");
-//         dadEl.textContent = dadJokeList[i];
-//         savedDadJokes.appendChild(dadEl); 
-//     }
-
-// 	let badJokeList = JSON.parse(localStorage.getItem("badJokeSave"))
-// 	for (let i = 0; i < badJokeList.length; i++) {
-//         const badEl = document.createElement("li");
-//         badEl.textContent = badJokeList[i];
-//         savedBadJokes.appendChild(badEl); 
-//     }
-// };
-
-// displaySaved();
-
-
-
 // --- Chuck Norris Fetch API URL --- //
 const options = {
 	method: 'GET',
@@ -201,7 +174,7 @@ function goFetch() {
 						}
 						levelText.innerHTML = communityLevel
 					}
-					levelPreface.innerHTML = 'CDC Community Level is '
+					levelPreface.innerHTML = 'Better get some advice from Chuck Norris (and a joke or two!) <br> <a href="./norris.html">-Link to the Norris Page-</a> <br> CDC Community Level is '
 					levelChanger()
 					console.log(communityLevel)
                      
